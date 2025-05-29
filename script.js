1ab20f4aeff7a1b50142ce6839e25d70a0cae731
YotamMolho <yotamolho@gmail.com>
2025-05-29 19:18:25 +0300

    wotk3-JS

---
+++ script.js
@@ -0,0 +1,59 @@
+
+let bgMusic; 
+
+document.addEventListener('DOMContentLoaded', function() { // הודעה קופצת בפתיחת העמוד
+  const modal = document.getElementById('welcome');
+  const btn = document.getElementById('startMusic');
+  modal.style.display = 'flex';
+
+  btn.addEventListener('click', function() { // מנגן בלחיצה על כפתור LET'S GO 
+    if (!bgMusic) {
+      bgMusic = new Audio('/sounds/jungle.mp3');
+      bgMusic.volume = 0.4;
+      bgMusic.play();
+    }
+    modal.style.display = 'none';
+  });
+});
+
+
+//השמעת קול של חיה 
+function playAnimalSound(jungle) {
+  const sound = new Audio(`sounds/${jungle}.mp3`);
+  sound.play();
+}
+
+// התאמה בין חיה לקול
+document.querySelectorAll('.animal').forEach(img => {
+  img.addEventListener('click', function() {
+    const animal = this.getAttribute('data-animal');
+    playAnimalSound(animal);
+  });
+});
+
+// כל אות משמיעה חיה מתאימה
+document.addEventListener('keydown', function(event) {
+  switch(event.key.toLowerCase()) {
+    case 'l':
+    case 'ל':  
+         playAnimalSound('lionSound'); break;         
+    case 'm':
+    case 'צ':
+         playAnimalSound('monkeySound'); break;
+    case 'p':
+    case 'פ':
+         playAnimalSound('parrotSound'); break;
+    case 'e':
+    case 'ק':
+         playAnimalSound('elephantSound'); break;
+    case 't':
+    case 'א':
+         playAnimalSound('tigerSound'); break;
+    case 's':
+    case 'ד':
+         playAnimalSound('snakeSound'); break;
+    case 'f':
+    case 'כ':
+         playAnimalSound('frogSound'); break;
+  }
+});
